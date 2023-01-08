4f1a2a8ea47caf55cacc40cb8eb447833b3a0bda
Jacob Wilson <104321419+SherllyNeo@users.noreply.github.com>
2023-01-08 05:47:14 +0000

    Init

---
+++ gol.js
@@ -0,0 +1,115 @@
+class GameOfLife {
+    constructor() {
+
+        this.cell_size = 20;
+        this.dead_colour = `#C0C0C0`;
+        this.alive_colour = `#FF8C69`;
+        this.cells_in_column = Math.floor(canvas.width / this.cell_size);
+        this.cells_in_rows = Math.floor(canvas.height / this.cell_size);
+        this.active_array = [];
+        this.inactive_array = [];
+
+        this.arrayInitialization = () => {
+
+            for (let i = 0; i < this.cells_in_rows; i++) {
+                this.active_array[i] = [];
+                for (let j = 0; j < this.cells_in_column; j++) {
+                    this.active_array[i][j] = 0;
+                }
+            }
+            this.inactive_array = this.active_array;
+
+        };
+
+        this.rand = () => {
+
+            for (let i = 0; i < this.cells_in_rows; i++) {
+                for (let j = 0; j < this.cells_in_column; j++) {
+                    this.active_array[i][j] = (Math.random() > 0.5) ? 1 : 0;
+                }
+            }
+
+        };
+
+        this.fillArray = () => {
+
+            for (let i = 0; i < this.cells_in_rows; i++) {
+                for (let j = 0; j < this.cells_in_column; j++) {
+                    let colour;
+                    if (this.active_array[i][j] == 1)
+                        colour = this.alive_colour;
+                    else
+                        colour = this.dead_colour;
+                    ctx.fillStyle = colour;
+                    ctx.fillRect(j * this.cell_size, i * this.cell_size, this.cell_size, this.cell_size);
+                }
+            }
+
+        };
+
+        this.setCellValueHelper = (row, col) => {
+            try {
+                return this.active_array[row][col];
+            }
+            catch {
+                let row_ = (row + this.cells_in_rows) % this.cells_in_rows;
+                let col_ =  (col + this.cells_in_column) % this.cells_in_column;
+                return this.active_array[row_][col_]
+            }
+           
+        };
+
+        this.countNeighbours = (row, col) => {
+            let total_neighbours = 0;
+            total_neighbours += this.setCellValueHelper(row - 1, col - 1);
+            total_neighbours += this.setCellValueHelper(row - 1, col);
+            total_neighbours += this.setCellValueHelper(row - 1, col + 1);
+            total_neighbours += this.setCellValueHelper(row, col - 1);
+            total_neighbours += this.setCellValueHelper(row, col + 1);
+            total_neighbours += this.setCellValueHelper(row + 1, col - 1);
+            total_neighbours += this.setCellValueHelper(row + 1, col);
+            total_neighbours += this.setCellValueHelper(row + 1, col + 1);
+            return total_neighbours;
+        };
+
+        this.updateCellValue = (row, col) => {
+
+            const total = this.countNeighbours(row, col);
+            // cell with more than 4 or less then 3 neighbours dies. 1 => 0; 0 => 0
+            if (total > 4 || total < 3) {
+                return 0;
+            }
+            // dead cell with 3 neighbours becomes alive. 0 => 1
+            else if (this.active_array[row][col] === 0 && total === 3) {
+                return 1;
+            }
+            // or returning its status back. 0 => 0; 1 => 1
+            else {
+                return this.active_array[row][col];
+            }
+
+        };
+
+        this.updateLifeCycle = () => {
+
+            for (let i = 0; i < this.cells_in_rows; i++) {
+                for (let j = 0; j < this.cells_in_column; j++) {
+                    let new_state = this.updateCellValue(i, j);
+                    this.inactive_array[i][j] = new_state;
+                }
+            }
+            this.active_array = this.inactive_array
+
+        };
+
+        this.gameSetUp = () => {
+            this.arrayInitialization();
+        };
+
+        this.runGame = () => {
+            this.updateLifeCycle();
+            this.fillArray();
+        };
+        
+    }
+}
---
+++ index.js
@@ -0,0 +1,20 @@
+const canvas = document.querySelector("#gamefield")
+const ctx = canvas.getContext("2d")
+
+const game = new GameOfLife();
+game.gameSetUp();
+window.onload = () => {
+
+    document.querySelector("#start-random").addEventListener("click", () => {
+        game.rand();
+        game.fillArray();
+        window.setInterval(() => {
+            game.runGame();
+        }, 300)
+    })
+
+    document.querySelector("#stop").addEventListener("click", () => {
+        game.gameSetUp();
+    })
+
+}
