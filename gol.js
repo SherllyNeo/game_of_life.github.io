e387e2a775355bbccaa62831e1b35d48bbb642ec
SherllyNeo <sherlly.neomail@gmail.com>
2023-01-10 22:09:51 +0000

    pretty colours

--- gol.js
+++ gol.js
@@ -1,3 +1,21 @@
+class Cell {
+    constructor() {
+        this.alive = false;
+        this.history = [];
+    
+        this.get_colour = () => {
+            let true_count = 0;
+            let hist = [...this.history];
+            while (hist.at(-1) == true) {
+                true_count++
+                hist.pop()
+            }
+            return true_count;
+        }
+}
+
+}
+
 class GameOfLife {
     constructor() {
 
@@ -8,12 +26,13 @@ class GameOfLife {
         this.cells_in_rows = Math.floor(canvas.height / this.cell_size);
         this.active_array = [];
         this.inactive_array = [];
+
      this.arrayInitialization = () => {
 
              for (let i = 0; i < this.cells_in_rows; i++) {
                  this.active_array[i] = [];
                  for (let j = 0; j < this.cells_in_column; j++) {
-                     this.active_array[i][j] = 0;
+                     this.active_array[i][j] = new Cell();
                  }
              }
 	
@@ -24,7 +43,10 @@ class GameOfLife {
 
             for (let i = 0; i < this.cells_in_rows; i++) {
                 for (let j = 0; j < this.cells_in_column; j++) {
-                    this.active_array[i][j] = (Math.random() > 0.5) ? 1 : 0;
+                    let binary = (Math.random() > 0.5)
+                    this.active_array[i][j].alive = binary;
+                    this.active_array[i][j].history = [].concat(this.active_array[i][j].history, binary);
+                    
                 }
             }
 
@@ -35,8 +57,31 @@ class GameOfLife {
             for (let i = 0; i < this.cells_in_rows; i++) {
                 for (let j = 0; j < this.cells_in_column; j++) {
                     let colour;
-                    if (this.active_array[i][j] == 1)
-                        colour = this.alive_colour;
+                    let cell = this.active_array[i][j]
+                    if (cell.alive) {
+                        let count = cell.get_colour()
+                        console.log("count:",cell.history)
+                        if (count == 0) {
+                            colour = this.alive_colour;
+                        }
+                        else if (count == 1) {
+                            colour = this.alive_colour;
+                        }
+                        else if (count == 2) {
+                            colour = `#465e7d`
+                        }
+                        else if (count == 3) {
+                            colour = `#446e66`
+                        }
+                        else if (count == 4) {
+                            colour  = `#990000`
+                        }
+                        else {
+                            colour = `#d5a6bd`
+                        }
+                        
+                    }
+                        
                     else
                         colour = this.dead_colour;
                     ctx.fillStyle = colour;
@@ -48,12 +93,22 @@ class GameOfLife {
 
         this.setCellValueHelper = (row, col) => {
             try {
-                return this.active_array[row][col];
+                if (this.active_array[row][col].alive) {
+                    return 1;
+                }
+                else {
+                    return 0;
+                }
             }
             catch {
                 let row_ = (row + this.cells_in_rows) % (this.cells_in_rows);
                 let col_ =  (col + this.cells_in_column) % (this.cells_in_column);
-                return this.active_array[row_][col_]
+                if (this.active_array[row_][col_].alive) {
+                    return 1;
+                }
+                else {
+                    return 0;
+                }
             }
            
         };
@@ -63,16 +118,16 @@ class GameOfLife {
           let current_state = this.active_array[row][col];
 
             if (total > 4 || total == 1) {
-                return 0;
+                return false;
             }
          
-            else if (total == 3 || (current_state == 1 && total == 2)) {
+            else if (total == 3 || (current_state.alive && total == 2)) {
                 console.log(row,col,"has",total,"neighbours")
-                return 1;
+                return true;
             }
    
             else {
-                return 0;
+                return false;
             }
 
         };
@@ -85,7 +140,7 @@ class GameOfLife {
             for (let i = 0; i <= this.cells_in_rows; i++) {
                 holder[i] = [];
                 for (let j = 0; j <= this.cells_in_column; j++) {
-                   holder[i][j] = 0;
+                   holder[i][j] = new Cell();
                 }
             }
 		
@@ -102,8 +157,9 @@ class GameOfLife {
                      let  neigh8 = this.setCellValueHelper(i + 1, j + 1);
                     let total_neigh = neigh1 + neigh2 + neigh3 + neigh4+ neigh5+ neigh6+ neigh7+ neigh8
                     let new_state = this.rules(total_neigh,i, j);
-                    holder[i][j] = new_state;
-                    if (new_state == 1) {
+                    holder[i][j].alive = new_state;
+                    holder[i][j].history = [].concat(this.active_array[i][j].history,new_state)
+                    if (new_state) {
                         console.log(i,j,"has been changed to",new_state)
                     }
                    
@@ -130,7 +186,8 @@ class GameOfLife {
             let row = Math.floor(((e.clientX - rect.left) / width) * this.cells_in_column)
             let col = Math.floor(((e.clientY - rect.top)/ height) * this.cells_in_rows)
 
-            this.active_array[col][row] = 1 - this.active_array[col][row];
+            this.active_array[col][row].alive = !this.active_array[col][row].alive;
+            this.active_array[col][row].history = [].concat(this.active_array[col][row].history, !this.active_array[col][row].history.alive);
             console.log(this.active_array.length,this.active_array[0].length,col,row,)
             
             this.fillArray();
